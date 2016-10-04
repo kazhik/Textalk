a16fcbe4cb14900f88c75099e7c5cb28d66bfbb7
Koike Kazuhiko <kazhik@gmail.com>
2016-10-05 04:23:01 +0900

    gradle

---
+++ app/src/main/java/net/kazhik/android/textalk/HandwritingActivity.java
@@ -0,0 +1,353 @@
+package net.kazhik.android.textalk;
+
+import java.io.File;
+import java.io.IOException;
+
+import net.kazhik.android.textalk.chat.ChatManager;
+import net.kazhik.android.textalk.chat.ChatService;
+import net.kazhik.android.textalk.chat.ChatService.ChatBinder;
+import android.app.Activity;
+import android.content.ComponentName;
+import android.content.Context;
+import android.content.Intent;
+import android.content.ServiceConnection;
+import android.content.pm.ActivityInfo;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.Point;
+import android.graphics.PointF;
+import android.os.Bundle;
+import android.os.IBinder;
+import android.util.Log;
+import android.view.Display;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.MotionEvent;
+import android.view.Surface;
+import android.view.View;
+import android.view.WindowManager;
+import android.widget.Button;
+import android.widget.Toast;
+
+
+public class HandwritingActivity extends Activity implements
+		View.OnTouchListener, ServiceConnection, ChatManager.ReceiveMessageListener {
+
+	private HandwritingView m_handwritingView;
+	private static final String TAG = "HandwritingActivity";
+
+	private Button m_undoBtn;
+	private Button m_clearBtn;
+	private Button m_sendBtn;
+	private ChatManager chatManager;
+	private String myname;
+
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.drawing_activity);
+
+		m_handwritingView = (HandwritingView) findViewById(R.id.handwritingView);
+		m_handwritingView.setOnTouchListener(this);
+
+		m_undoBtn = (Button) findViewById(R.id.undoBtn);
+		m_clearBtn = (Button) findViewById(R.id.clearBtn);
+		m_sendBtn = (Button) findViewById(R.id.sendBtn);
+
+		Bitmap bmp = null;
+		Intent intent = this.getIntent();
+//		String sender = intent.getStringExtra("sender");
+		String filename = intent.getStringExtra("bitmap");
+		if (filename != null) {
+			bmp = BitmapFactory.decodeFile(filename);
+			new File(filename).delete();
+			if (bmp != null) {
+				m_handwritingView.setBitmap(bmp);
+			}
+		}
+		this.myname = intent.getStringExtra("myname");
+
+		m_undoBtn.setEnabled(false);
+		m_sendBtn.setEnabled(false);
+	}
+
+	private void disableButtons() {
+		m_undoBtn.setEnabled(false);
+		m_clearBtn.setEnabled(false);
+		m_sendBtn.setEnabled(false);
+	}
+
+	// http://stackoverflow.com/questions/3611457/android-temporarily-disable-orientation-changes-in-an-activity
+	private void lockOrientation() {
+		Display display = getWindowManager().getDefaultDisplay();
+		int rotation = display.getRotation();
+		int height;
+		int width;
+		Point size = new Point();
+		display.getSize(size);
+		height = size.y;
+		width = size.x;
+		switch (rotation) {
+		case Surface.ROTATION_90:
+			if (width > height)
+				setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE);
+			else
+				setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_REVERSE_PORTRAIT);
+			break;
+		case Surface.ROTATION_180:
+			if (height > width)
+				setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_REVERSE_PORTRAIT);
+			else
+				setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_REVERSE_LANDSCAPE);
+			break;
+		case Surface.ROTATION_270:
+			if (width > height)
+				setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_REVERSE_LANDSCAPE);
+			else
+				setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
+			break;
+		default:
+			if (height > width)
+				setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
+			else
+				setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE);
+		}
+	}
+
+	@Override
+	public boolean onTouch(View view, MotionEvent motionEvent) {
+		PointF pt = new PointF(motionEvent.getX(), motionEvent.getY());
+
+		if (motionEvent.getAction() == MotionEvent.ACTION_DOWN) {
+			m_handwritingView.touchDown(pt);
+		} else if (motionEvent.getAction() == MotionEvent.ACTION_MOVE) {
+			m_handwritingView.touchMove(pt);
+		} else if (motionEvent.getAction() == MotionEvent.ACTION_UP) {
+			m_handwritingView.touchUp(pt);
+
+			m_undoBtn.setEnabled(true);
+			m_clearBtn.setEnabled(true);
+
+            if (this.chatManager.getConnectionCount() > 0) {
+                m_sendBtn.setEnabled(true);
+            }
+		}
+		//disableRotation();
+		this.lockOrientation();
+
+		return true;
+	}
+
+	public void onClick(View view) {
+		switch (view.getId()) {
+		case R.id.undoBtn:
+			m_handwritingView.undo();
+			if (m_handwritingView.hasStack() == false) {
+				this.disableButtons();
+				setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_SENSOR);
+			}
+			break;
+
+		case R.id.clearBtn:
+			m_handwritingView.clear();
+			this.disableButtons();
+			setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_SENSOR);
+			break;
+//		case R.id.closeBtn:
+//			m_drawingSurface.stopDraw();
+//			finish();
+//			break;
+		
+		case R.id.sendBtn:
+			this.broadcastBitmap();
+			break;
+		
+		}
+	}
+	private void broadcastBitmap() {
+		Bitmap bmp = this.m_handwritingView.getBitmap();
+		try {
+			boolean bSent = this.chatManager.broadcastBitmap(bmp);
+			if (bSent) {
+				Toast.makeText(this, R.string.bitmap_sent,
+						Toast.LENGTH_LONG).show();
+			} else {
+				Toast.makeText(this, R.string.not_connected,
+						Toast.LENGTH_LONG).show();
+			}
+		} catch (IOException e) {
+			Log.e(TAG, "Failed to broadcast bitmap", e);
+		}
+		
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		menu.add(Menu.NONE, Constants.MENU_SETTING, Menu.NONE,
+				R.string.menu_settings).setIcon(
+				android.R.drawable.ic_menu_preferences);
+		return super.onCreateOptionsMenu(menu);
+	}
+
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item) {
+		boolean ret = true;
+		Intent intent;
+		switch (item.getItemId()) {
+		case Constants.MENU_SETTING:
+			intent = new Intent(this, Config.class);
+			intent.setAction(Intent.ACTION_VIEW);
+			startActivity(intent);
+			ret = true;
+			break;
+		default:
+			ret = super.onOptionsItemSelected(item);
+			break;
+		}
+		return ret;
+	}
+	@Override
+	protected void onStart() {
+		super.onStart();
+		
+		Log.d(TAG, "bind service");
+		Intent intent = new Intent(this, ChatService.class);
+
+		this.bindService(intent, this, Context.BIND_AUTO_CREATE);
+	}
+	@Override
+	protected void onStop() {
+		super.onStop();
+		Log.d(TAG, "unbind service");
+		this.unbindService(this);
+	}
+	@Override
+	protected void onPause() {
+		super.onPause();
+		if (this.chatManager != null) {
+			this.chatManager.pause();
+		}
+	}
+	@Override
+	protected void onResume() {
+		super.onResume();
+		if (this.chatManager != null) {
+			this.chatManager.resume();
+		}
+
+	}
+	@Override
+	public void onServiceConnected(ComponentName name, IBinder service) {
+		Log.d(TAG, "onServiceConnected: " + name.toString());
+		ChatBinder binder = (ChatBinder)service;
+		this.chatManager = binder.getChatManager();
+		this.chatManager.init(this.myname);
+		this.chatManager.addReceiveMessageListener(this);
+		if (this.chatManager.getConnectionCount() > 0) {
+			m_sendBtn.setEnabled(true);
+		} else {
+			m_sendBtn.setEnabled(false);
+		}
+
+	}
+	@Override
+	public void onServiceDisconnected(ComponentName name) {
+		Log.d(TAG, "onServiceDisConnected: " + name.toString());
+		
+	}
+	private void setSendButtonEnabled(boolean enable) {
+		class SetSendButtonEnabled implements Runnable {
+			private boolean enable;
+			
+			public SetSendButtonEnabled(boolean enable) {
+				this.enable = enable;
+			}
+			@Override
+			public void run() {
+				m_sendBtn.setEnabled(enable);
+			}
+			
+		};
+
+		this.runOnUiThread(new SetSendButtonEnabled(enable));
+	}
+
+	@Override
+	public void onConnected(String ipaddr, String name) {
+		this.toast(this.getResources().getString(R.string.connected, name));
+		this.setSendButtonEnabled(true);
+		getWindow().addFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON);
+	}
+
+	@Override
+	public void onRenamed(String oldname, String newname) {
+		this.toast(this.getResources().getString(R.string.renamed, oldname, newname));
+		
+	}
+
+	@Override
+	public void onDisconnected(String ipaddr, String name) {
+		this.toast(this.getResources().getString(R.string.disconnected, name));
+		if (this.chatManager.getConnectionCount() == 0) {
+			this.setSendButtonEnabled(false);
+			getWindow().clearFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON);
+		}
+	}
+	private void toast(String msg) {
+		class ShowToast implements Runnable {
+			private String msg;
+			public ShowToast(String msg) {
+				this.msg = msg;
+			}
+
+			@Override
+			public void run() {
+				
+				Toast.makeText(HandwritingActivity.this,
+						this.msg,
+						Toast.LENGTH_LONG).show();
+				
+			}
+			
+		};
+		this.runOnUiThread(new ShowToast(msg));
+		
+	}
+
+	@Override
+	public void onMessage(String sender, String text) {
+		Intent intent = new Intent();
+		intent.putExtra("sender", sender);
+		intent.putExtra("text", text);
+		this.setResult(RESULT_OK, intent);
+		this.finish();
+	}
+	private void showBitmap(String sender, String filename) {
+		class ShowBitmap implements Runnable {
+			private Bitmap bmp;
+			
+			public ShowBitmap(Bitmap bmp) {
+				this.bmp = bmp;
+			}
+
+			@Override
+			public void run() {
+				m_handwritingView.setBitmap(this.bmp);
+				m_handwritingView.invalidate();
+				setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_SENSOR);
+				
+			}
+			
+		};
+		Bitmap bmp = BitmapFactory.decodeFile(filename);
+		new File(filename).delete();
+
+		this.runOnUiThread(new ShowBitmap(bmp));
+	}
+
+
+	@Override
+	public void onBitmap(String name, String filename) {
+		this.showBitmap(name, filename);
+	}
+}
---
+++ app/src/main/java/net/kazhik/android/textalk/chat/UdpManager.java
@@ -0,0 +1,143 @@
+package net.kazhik.android.textalk.chat;
+
+
+import java.math.BigInteger;
+import java.net.InetAddress;
+import java.net.UnknownHostException;
+import java.nio.ByteOrder;
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.TimeUnit;
+
+import android.content.Context;
+import android.net.wifi.WifiInfo;
+import android.net.wifi.WifiManager;
+import android.util.Log;
+
+public class UdpManager implements UdpReceiver.MessageListener {
+
+	private ScheduledExecutorService m_sender =
+			Executors.newScheduledThreadPool(1);
+	private ExecutorService m_receiver =
+			Executors.newSingleThreadExecutor();
+
+	public static final int PORT = 5057;
+	private InetAddress localAddr;
+	private Map<String, Long> m_remoteAddrs =
+			new ConcurrentHashMap<String, Long>();
+	private UdpReceiver receiveTask = null;
+	private static final int BUFFSIZE = 128;
+	private static final String TAG = "UdpManager";
+	private ConnectionListener m_listener;
+	
+	public void start(Context context, String sendData, ConnectionListener listener) {
+		m_listener = listener;
+		
+		WifiManager wifiManager =
+				(WifiManager)context.getSystemService(Context.WIFI_SERVICE);
+
+		WifiInfo wifiInfo = wifiManager.getConnectionInfo();
+		this.localAddr = this.convertIpAddr(wifiInfo.getIpAddress());
+		if (this.localAddr == null) {
+			Log.e(TAG, "Failed to get My Address");
+			return;
+		}
+		Log.i(TAG, "My Address is: " + this.localAddr.getHostAddress());
+		this.startReceive();
+		this.startRefresh();
+		this.startBroadcasting(sendData);
+	}
+	public void stop() {
+		m_sender.shutdown();
+		if (this.receiveTask != null) {
+			this.receiveTask.close();
+		}
+		m_remoteAddrs.clear();
+		m_receiver.shutdown();
+	}
+	// http://stackoverflow.com/questions/16730711/get-my-wifi-ip-address-android
+	private InetAddress convertIpAddr(int ipAddress) {
+		// Convert little-endian to big-endianif needed
+		if (ByteOrder.nativeOrder().equals(ByteOrder.LITTLE_ENDIAN)) {
+			ipAddress = Integer.reverseBytes(ipAddress);
+		}
+
+		byte[] ipByteArray = BigInteger.valueOf(ipAddress).toByteArray();
+
+		InetAddress addr = null;
+		try {
+			addr = InetAddress.getByAddress(ipByteArray);
+		} catch (UnknownHostException ex) {
+			Log.e(TAG, "getLocalWifiAddress", ex);
+		}
+
+		return addr;
+	}
+	private void refreshRemoteAddrs() {
+		long currTime = System.currentTimeMillis();
+		for (String addr: m_remoteAddrs.keySet()) {
+			long lastUpdateTime = m_remoteAddrs.get(addr);
+			if (currTime - lastUpdateTime > 20 * 1000) {
+				Log.i(TAG, addr + " is dead");
+				m_listener.onHostDead(addr);
+				m_remoteAddrs.remove(addr);
+			}
+		}
+	}
+	private void startRefresh() {
+		Runnable r = new Runnable() {
+			@Override
+			public void run() {
+				refreshRemoteAddrs();
+			}
+			
+		};
+		this.m_sender.scheduleAtFixedRate(r, 10, 2, TimeUnit.SECONDS);
+	}
+
+
+	private void startReceive() {
+		if (this.receiveTask == null) {
+			this.receiveTask = new UdpReceiver(this);
+			this.receiveTask.init(PORT, BUFFSIZE);
+			
+			m_receiver.submit(this.receiveTask);
+		}
+
+		
+	}
+	private void startBroadcasting(String sendData) {
+		int sendLen = sendData.getBytes().length;
+		if (sendLen > BUFFSIZE) {
+			Log.e(TAG, "senddata too big: " + sendLen);
+			return;
+		}
+		UdpBroadcast broadcast = new UdpBroadcast();
+		boolean ret = broadcast.init(this.localAddr, PORT, sendData);
+		if (!ret) {
+			return;
+		}
+		
+		// broadcasts in every 10 seconds
+		m_sender.scheduleAtFixedRate(broadcast, 0, 10, TimeUnit.SECONDS);
+
+	}
+	
+	@Override
+	public void onReceived(InetAddress sender, String msg) {
+		if (sender.equals(this.localAddr)) {
+			return;
+		}
+
+		String addr = sender.getHostAddress();
+		if (!m_remoteAddrs.containsKey(addr)) {
+			m_listener.onNewHost(addr, msg);
+		}
+		this.m_remoteAddrs.put(addr, System.currentTimeMillis());
+		Log.i(TAG, "Received from " + addr + ": " + msg);
+	}
+
+}
